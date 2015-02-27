8fc7dfbe074bfa7ac5eac68492c532f35f007452
moophy <MoophyAlan@gmail.com>
2015-02-28 00:31:19 +1300

    there is problem in the structrue of the tree 	modified:   ../huffman/main.cpp

--- cpr/huffman/main.cpp
+++ cpr/huffman/main.cpp
@@ -3,18 +3,20 @@
 
 int main()
 {
-	std::vector<char> raw_data{ 'a', 'a', 'a', 'a', 'b', 'b', 'c' };
-	std::vector<cpr::huffman::Node<int>> C;
-	for (auto ch : raw_data)
-	{
-		auto it = std::find_if(C.begin(), C.end(), [ch](cpr::huffman::Node<int> const& node){return node.character_ == ch; });
-		if (it != C.cend())
-			++it->freq_;
-		else
-			C.push_back(cpr::huffman::Node<int>(ch, 1));
-	}
-	std::cout << "size of Set C : " << C.size() << std::endl;
+	//std::vector<char> raw_data{ 'a', 'a', 'a', 'a', 'b', 'b', 'c' };
+	//std::vector<cpr::huffman::Node<int>> C;
+	//for (auto ch : raw_data)
+	//{
+	//	auto it = std::find_if(C.begin(), C.end(), [ch](cpr::huffman::Node<int> const& node){return node.character_ == ch; });
+	//	if (it != C.cend())
+	//		++it->freq_;
+	//	else
+	//		C.push_back(cpr::huffman::Node<int>(ch, 1));
+	//}
+	//std::cout << "size of Set C : " << C.size() << std::endl;
 
+	using Char = cpr::huffman::Node<int>;
+	std::vector<cpr::huffman::Node<int>> C{ Char('a', 45), Char('b', 13), Char('c', 12), Char('d', 16), Char('e', 9), Char('f', 5) };
 	cpr::huffman::HuffmanTree<int> huff_tree(C.cbegin(), C.cend());
 	huff_tree.print();
 	return 0;
