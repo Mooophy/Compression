6a906ee75e4a2b3570837060561afa142a467ad6
moophy <MoophyAlan@gmail.com>
2015-03-01 10:13:52 +1300

    tested buffer class, fixed a bug (forgot to maintain current bit index)

--- cpr/huffman/buffer.hpp
+++ cpr/huffman/buffer.hpp
@@ -17,10 +17,19 @@ namespace cpr
 				: curr_bit_index_{ msb_index() }, data_{}
 			{	}
 
+			Buffer(std::initializer_list<T> li)
+				: curr_bit_index_{ msb_index() }, data_(li)
+			{	}
+
 			template<typename Iterator>
 			Buffer(Iterator first, Iterator last)
 				: curr_bit_index_{ msb_index() }, data_(first, last)
 			{	}
+
+			void push_back(T const& val)
+			{
+				data_.push_back(val);
+			}
 
 			std::list<T> const& data() const
 			{
@@ -33,10 +42,11 @@ namespace cpr
 			unsigned pop_front_bit()
 			{
 				unsigned msb = (data_.front() & (0x1 << curr_bit_index_)) >> curr_bit_index_;
-
-				if (--bit_index_ < 0)
+				if (--curr_bit_index_ < 0)
+				{
 					data_.pop_front();
-
+					curr_bit_index_ = msb_index();
+				}
 				return msb;
 			}
 
--- cpr/huffman/main.cpp
+++ cpr/huffman/main.cpp
@@ -4,20 +4,24 @@
 
 int main()
 {
+	///////////test for huffman node and tree
 	using Char = cpr::huffman::Node<int>;
 	std::vector<Char> C{ Char('a', 45), Char('b', 13), Char('c', 12), Char('d', 16), Char('e', 9), Char('f', 5) };
 	cpr::huffman::HuffmanTree<int> huff_tree(C.cbegin(), C.cend());
 	huff_tree.print();
 
-	std::cout << "\nfor encoding : \n";
+	///////////test for encoding
+	std::cout << "\n--test for encoding : \n";
 	for (auto ch : C)
 		std::cout << "the code for char " << ch.character_ << " is " << huff_tree.encode(ch.character_) << std::endl;
 
-
 	////////////test for buffer
+	std::cout << "\n--test for buffer\n";
+	cpr::huffman::Buffer<unsigned char> buff{ 0x31, 0xff, 0x00 };
+	while (!buff.data().empty())
+		std::cout << buff.pop_front_bit();
 
-	cpr::huffman::Buffer<char> buff;
-
-	std::cout << "\nexit normally" << std::endl;
+	std::cout << std::endl;
+	system("pause");
 	return 0;
 }
