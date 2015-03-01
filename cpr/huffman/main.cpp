cc686b1a1a04312eaf4de9d67096ea5618b55fba
moophy <MoophyAlan@gmail.com>
2015-03-01 14:13:27 +1300

    modified:   cpr/huffman/huffman.hpp 	modified:   cpr/huffman/main.cpp

--- cpr/huffman/main.cpp
+++ cpr/huffman/main.cpp
@@ -10,11 +10,6 @@ int main()
 	cpr::huffman::HuffmanTree<int> huff_tree(C.cbegin(), C.cend());
 	huff_tree.print();
 
-	///////////test for encoding
-	//std::cout << "\n--test for encoding : \n";
-	//for (auto ch : C)
-	//	std::cout << "the code for char " << ch.character_ << " is " << huff_tree.encode(ch.character_) << std::endl;
-
 	////////////test for buffer
 	std::cout << "\n--test for buffer\n";
 	cpr::huffman::Buffer<unsigned char> buff{ 0x31, 0xff, 0x00 };
