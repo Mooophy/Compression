b55bced84cda15bd315d091a214ef90e3af57682
moophy <MoophyAlan@gmail.com>
2015-03-04 12:40:10 +1300

    add ctor

--- cpr/huffman/code_word_dictionary.hpp
+++ cpr/huffman/code_word_dictionary.hpp
@@ -11,9 +11,32 @@ namespace cpr
 		template<typename Char, typename Freq, typename CodeWord>
 		class CodeWordDictionary : public std::map < Char, CodeWord >
 		{
+			using std::map < Char, CodeWord >::map;
+			using SharedNode = typename cpr::huffman::Node<Char, Freq>::SharedNode;
 		public:
-		private:
+			explicit CodeWordDictionary(HuffmanTree<Char, Freq> const& htree)
+				: map()
+			{
+				std::function<void(CodeWord, SharedNode)> 
+					fill_dic_by_dfs = [=this](CodeWord path, SharedNode node)
+				{
+					if (node)
+					{
+						if (node->character_ != 0)
+						{
+							(*this)[node->character_] = path;
+							return;
+						}
+						else
+						{
+							fill_dic_by_dfs(path << 1 + 0, node->left_);
+							fill_dic_by_dfs(path << 1 + 1, node->right_);
+						}
+					}
 
+					fill_dic_by_dfs(0, htree.root());
+				}
+			}
 		};
 	}
 }
