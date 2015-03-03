b30d02ce60bc8f4ebb535f6ab8418f72fa644657
moophy <MoophyAlan@gmail.com>
2015-03-03 16:47:56 +1300

    use std::map for frequency table instead

--- cpr/huffman/huffman_tree.hpp
+++ cpr/huffman/huffman_tree.hpp
@@ -18,35 +18,35 @@ namespace cpr
 		public:
 			using SharedNode = Node<Char, Freq>::SharedNode;
 
-			template<typename Container>
-			explicit HuffmanTree(Container const& nodes)
-				: root_{ make_tree(nodes) }
-			{	}
+			//template<typename Container>
+			//explicit HuffmanTree(Container const& nodes)
+			//	: root_{ make_tree(nodes) }
+			//{	}
 
 		private:
 			SharedNode root_;
 
-			template<typename Container>
-			SharedNode make_tree(Container const& nodes) const
-			{
-				auto greater = [](SharedNode lhs, SharedNode rhs){return lhs->freq_ > rhs->freq_; };
-				using MinPriorityQueue = std::priority_queue < SharedNode, std::vector<SharedNode>>, decltype(greater) > ;
+			//template<typename Container>
+			//SharedNode make_tree(Container const& nodes) const
+			//{
+			//	auto greater = [](SharedNode lhs, SharedNode rhs){return lhs->freq_ > rhs->freq_; };
+			//	using MinPriorityQueue = std::priority_queue < SharedNode, std::vector<SharedNode>>, decltype(greater) > ;
 
-				MinPriorityQueue queue(greater);
-				for (auto const& n : node)
-					queue.push(make_new_node(n));
+			//	MinPriorityQueue queue(greater);
+			//	for (auto const& n : node)
+			//		queue.push(make_new_node(n));
 
-				for (int _ = 1; _ != container.size(); ++_)
-				{
-					auto z = make_new_node();
-					z->left_ = make_new_node(queue.top());
-					queue.pop();
-					z->right_ = make_new_node(queue.top());
-					queue.pop();
-					z.freq_ = z.left_->freq_ + z.right_->freq_;
-					queue.push(z);
-				}
-			}
+			//	for (int _ = 1; _ != container.size(); ++_)
+			//	{
+			//		auto z = make_new_node();
+			//		z->left_ = make_new_node(queue.top());
+			//		queue.pop();
+			//		z->right_ = make_new_node(queue.top());
+			//		queue.pop();
+			//		z.freq_ = z.left_->freq_ + z.right_->freq_;
+			//		queue.push(z);
+			//	}
+			//}
 		};
 	}
 }
