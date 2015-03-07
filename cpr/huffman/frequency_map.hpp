714e7cd2f066d4dd6af45472baf9ad40aa6b6e06
moophy <MoophyAlan@gmail.com>
2015-03-07 23:19:16 +1300

    add and tested FrequencyMap::str 	modified:   cpr/huffman/frequency_map.hpp 	modified:   cpr/unit_test_for_huffman/unit_test_for_frequency_map.cpp

--- cpr/huffman/frequency_map.hpp
+++ cpr/huffman/frequency_map.hpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <deque>
 
 #ifndef FREQUENCY_MAP_HPP
 #define FREQUENCY_MAP_HPP
@@ -22,6 +23,31 @@ namespace cpr
 						++(*this)[ch];
 					else
 						(*this)[ch] = 1;
+			}
+
+			//format : symbol:frequency,
+			//say :
+			//		a:1304,b:89,c:990,
+			std::string str()const
+			{
+				std::string str;
+				for (auto pair : *this)
+				{
+					str.push_back(pair.first);
+					str.push_back(':');
+					str += frequency_to_string(pair.second);
+					str.push_back(',');
+				}
+				return str;
+			}
+
+		private:
+			std::string frequency_to_string(Freq freq) const
+			{
+				std::deque < char > dq;
+				for (char ch = 0; freq > 0; freq >>= 8)
+					dq.push_front(freq & 0xff);
+				return std::string(dq.begin(), dq.end());
 			}
 		};
 	}
--- cpr/unit_test_for_huffman/unit_test_for_frequency_map.cpp
+++ cpr/unit_test_for_huffman/unit_test_for_frequency_map.cpp
@@ -22,5 +22,22 @@ namespace unit_test_for_huffman
 			Assert::AreEqual(1l, fmap.at('c'));
  		}
 
+		TEST_METHOD(str_case0)
+		{
+			std::vector<char> test_case{ 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'c' };
+			cpr::huffman::FrequencyMap<char, long> fmap(test_case);
+			std::string expect{ 'a', ':', 4, ',', 'b', ':', 3, ',', 'c', ':', 1, ',' };
+			Assert::AreEqual(expect, fmap.str());
+		}
+
+		TEST_METHOD(str_case1)
+		{
+			std::string test_case;
+			for (long i = 0; i != 0xabc; ++i)
+				test_case += "ab";
+			cpr::huffman::FrequencyMap<char, long> fmap(test_case);
+			std::string expect{ 'a', ':', (char)0x0a, (char)0xbc, ',', 'b', ':', (char)0x0a, (char)0xbc,',' };
+			Assert::AreEqual(expect, fmap.str());
+		}
 	};
 }
